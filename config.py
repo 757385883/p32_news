b897b3f604a813bde4f67f54fdda2a894e4ea46d
pyc <pyc130721@163..com>
2019-03-27 15:46:19 +0800

    项目目录&SQLACHEMY和数据迁移&数据库连接

--- config.py
+++ config.py
@@ -1,16 +1,18 @@
+# 2.2定义配置信息类
 class Config:
     DEBUG = True
-
-
-# 开发模式下的配置信息
+# 4.3配置数据库的连接和动态追踪
+    SQLACHEMY_DATABASE_URI = 'mysql://root"mysql@lacalhost/info'
+    SQLACHEMY_TRACK_MODIFYCATIONS = False
+# 2.3开发模式下的配置信息
 class developmentConfig(Config):
     DEBUG = True
 
-#   生产模式下的配置信息
+#   2.4生产模式下的配置信息
 class productionConfig(Config):
     DEBUG =False
 
-# 定义一个字典，用来存储 配置模式的键值对，便于被导入，不同的模式
+# 2.5定义一个字典，用来存储 配置模式的键值对，便于被导入，不同的模式
 
 config ={
     'development' : developmentConfig,
--- manage.py
+++ manage.py
@@ -3,7 +3,10 @@ from flask import Flask
 from flask_script import Manager
 # 2.导入配置文件中的字典，
 from config import config
-
+# 3.导入sqlachmy ，实例化对象，定义模型类
+from flask_sqlalchemy import SQLAlchemy
+# 4.导入数据迁移
+from flask_migrate import Migrate,MigrateCommand
 app =Flask(__name__)
 
 # 1.1实例化管理器对象
@@ -11,7 +14,12 @@ manage = Manager(app)
 
 # 2.1使用配置信息
 app.config.from_object(config['development'])
-
+# 3.1实例化sqlachemy 对象
+db = SQLAlchemy(app)
+# 4.1使用迁移框架
+Migrate(app,db)
+# 4.2添加迁移命令
+manage.add_command('db',MigrateCommand)
 @app.route('/')
 def index():
     return 'index2018'
